ad42fb5d78ecbcda7ce390f27b2299cdea6aa6e9
Gilberto29ap <gilberto29ap@gmail.com>
2022-10-22 08:01:11 -0300

    Alteração em testes

--- 01 - Estrutura de dados/05 - Funções/testes.py
+++ 01 - Estrutura de dados/05 - Funções/testes.py
@@ -1,26 +1,36 @@
-frase = "Esta é a frase inicial"
-lista1 = [0,0]
-
-def iteracao_deep(frase):
-    for i in range(3):
-        frase += f"\n linha da iteração {i+1}"
-    return frase
-        
-
-def iteracao_lista(lista1):
-    for i in range (3):
-        lista1.append(i+3)
+from numpy import true_divide
 
 
+print("Bem vindo ao adivinhador de números")
 
-def main(frase, lista1):
-    iteracao_lista(lista1)
-    frase = iteracao_deep(frase)
-    print(f"Abaixo a frase após iteração\n{frase}")
-    print(lista1)
+def checagem_numero(numero):
+    checagem_numero = numero > 0 or numero is int
+    return  checagem_numero
 
-main(frase, lista1)
 
+numero_original = input("Digite um número para eu adivinhar:")
+
+
+if checagem_numero(numero_original):
+    #limpar tela
+    print("Ok armazenei o número, agora peça para outra pessoa adivinhar")
+    while True:
+        novo_numero = input("Adivinhe o número:")
+        if novo_numero == numero_original:
+            print("Uhuuu! Você acertou!")
+
+        elif checagem_numero(novo_numero):
+            diferenca_numeros = numero_original - novo_numero if numero_original > novo_numero else novo_numero - numero_original
+            temperatura = "muito quente!" if diferenca_numeros < 10 else "quente" if diferenca_numeros < 50 else "frio"
+            altura = "mais baixo" if novo_numero > numero_original else "mais alto"
+            print(f"Está {temperatura}!, tente um número mais {altura}")
+            
+        else:
+            print("Número inválido, tente novamente:")
+
+    
+else:
+    print("Número inválido, tente outra vez")
 
 
 
