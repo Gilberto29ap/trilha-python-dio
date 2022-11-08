31c87bf3c4dd5227f6293578c4dfa703afec4f26
Gilberto29ap <gilberto29ap@gmail.com>
2022-11-07 21:28:25 -0300

    Envio codigo desafio Python

--- 02 - Programação Orientada a Objetos/04 - Herança/teste_herança.py
+++ 02 - Programação Orientada a Objetos/04 - Herança/teste_herança.py
@@ -1,3 +1,36 @@
+class Ambiente:
+    def __init__(self, dens_att, temperatura):
+        self.dens_att = dens_att
+        self.temperatura = temperatura
+        def calc_coeficiente(self):
+# calculando dens att para coeficiente, quanto maior o valor, menor o arrasto, base 0
+            coeff_dens_att = -self.dens_att/1000
+# calculando temperatura para coeficiente, quanto maior o valor, menos arrasto, base 25       
+            coeff_temperatura = (25-self.temperatura)/1000
+
+            ca = 1 + coeff_dens_att + coeff_temperatura
+
+            return ca
+
+        self.coeffiente_ambiente = calc_coeficiente(self)
+
+# # calcular coeficiente de ambiente, quanto maior o valor maior o arrasto, float com 3 casas decimais aproximado de 1.000
+#     def calc_coeficiente(self):
+# # calculando dens att para coeficiente, quanto maior o valor, menor o arrasto, base 0
+#         coeff_dens_att = -self.dens_att/1000
+# # calculando temperatura para coeficiente, quanto maior o valor, menos arrasto, base 25       
+#         coeff_temperatura = (25-self.temperatura)/1000
+
+#         ca = 1 + coeff_dens_att + coeff_temperatura
+        
+#         print(f"calculando coeficiente de ambiente:  {ca}")
+
+#         return ca
+
+    
+
+
+
 class arma:
     def __init__(self, peso = 300, comprimento = 23):
         self.peso = peso
@@ -37,4 +70,8 @@ pistola1 = arma_fogo(1,".40", peso=400, comprimento = 20)
 
 pt100 = Pistola(capacidade_carregador=10, calibre=".40", peso=400, comprimento = 25, espaco_ocupado=5)
 
-print(pt100)
+print(pt100 )
+
+sertao = Ambiente(200, 40)
+
+print(sertao.coeffiente_ambiente)
